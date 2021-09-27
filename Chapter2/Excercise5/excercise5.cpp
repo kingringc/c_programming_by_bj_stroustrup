63c9a15128a5fce70d3e1e05478e760a912ac71e
Chandrasekar Arunachalam <kingring23@hotmail.com>
2021-09-27 10:28:53 +0530

    Excercise 5.

---
+++ Chapter2/Excercise5/excercise5.cpp
@@ -0,0 +1,55 @@
+// Prints Terms from c++ basic vocabulary
+
+#include "../../std_lib_facilities.h"
+
+int main() {
+	// Term
+	cout << "\n// is a double slash, Anything written after this token // considered as a comment.";
+	cout << " Comments are ignored by compiler, and written for the benefit of programmers who read the code.";
+	cout << " Comments are written to describe what the program is intended to do and in general to provide information";
+	cout << " useful for humans that can't be directly expressed in code.\n\n";
+	// <<
+	cout << "<< is a output operator \'put-into\' std cout to display value in monitor or console.\n\n";
+	// compiler
+	cout << "Compiler translates human redable code or source code in to executable or object code or machine code";
+	cout << " which machine/computer can understand.\n\n";
+	// compile-time error
+	cout << "Erros found by compiler are compile-time error\n\n";
+	// cout
+	cout << "cout refers to a standard output stream.";
+	cout << " cout is a abbrevation of \'Character Ouput Stream\'.\n\n";
+	// function
+	cout << "function is a named sequence of instructions for the computer to execute in order which they are written.";
+	cout << " function has fours parts as follows, return type, name, paramter list enclosed in parentheses ( ), function body";
+	cout << " enclosed with in pair of curly braces { }.\n\n";
+	// header
+	cout << "A file included using #include with suffix .h is called header file, header file contains definition of terms,";
+	cout << " such as cout.\n\n";
+	// executable
+	cout << "Final output of compiler is executable ot object code ot machine code, which a computer a run or execute on demand.\n\n";
+	// IDE
+	cout << "IDE is Integrated/Interactive Development Environment is a editor with all features to build, run, scan, spellchek used by many software professionals.\n\n";
+	// #include
+	cout << "#include directive instructs the computer to make available (to include) facilities from ****.h or within <> angle brackets.\n\n";
+	// library
+	cout << "Library is some code written by other developers that we access using #include ***.h\n\n";
+	// linker
+	cout << "Linker takes care of linking of compiled object code units to form a executable.";
+	cout << " A program usally consists of several units, often developed by different people,";
+	cout << " for example \'Hello World!\' program that we wrote plus the std library, these two separate parts must be compiled";
+	cout << " and the resulting object codes linked together to form an executable file/program.\n\n";
+	// main
+	cout << "Main is a special function, every c++ executable program must have, to tell it where to start executing.\n\n";
+	// Object code
+	cout << "Object code is output of compilation process for Linux and Unix systems.\n\n";
+	//output
+	cout << "An output is a result of any program, often expected after running the program.\n\n";
+	// program
+	cout << "To get computer to do something, someone or you must have to tell it exactly what to do in a way that computer can understand,";
+	cout << " such a description is call program.\n\n";
+	// source code
+	cout << "Source code a program text unit which a compiler can understand usally its a human redable code.\n\n";
+	// statement
+	cout << "A part of program that specifies an action is called a statement.\n\n";
+	return 0;
+}
